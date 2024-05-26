dfae7692924ae558ec32fca1653c5bb039807061
RafsanTanveer <rzrt.edu@gmail.com>
2024-05-26 14:08:31 +0600

    Add login screen

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,31 +1,137 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class Loginscreen extends StatelessWidget {
-  const Loginscreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('This is login screen',style: TextStyle(backgroundColor: Colors.green),),
-              ),
-            ),
-            OutlinedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/areasetting");
-                },
-                child: Text('Login screen'))
-          ],
+      appBar: AppBar(
+        title: Text(
+          'Galaxy Pharma App',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+          ),
         ),
-      )),
+        backgroundColor: Colors.greenAccent[400],
+        actions: [
+          IconButton(
+            icon: Image.asset('assets/images/alifsoftlogo.jpg'),
+            onPressed: () => {},
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.all(14),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _header(context),
+              _inputField(context),
+              _forgotPassword(context),
+              _signup(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _header(context) {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: Colors.purple,
+        border: Border.all(),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(15.0),
+          bottomRight: Radius.circular(15.0),
+        ),
+      ), //             <--- BoxDecoration here
+      child: Text(
+        "Galaxy Pharma ERP",
+        style: TextStyle(
+            fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.w900),
+      ),
+    );
+  }
+
+  _inputField(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+              hintText: "User Id",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18),
+                  borderSide: BorderSide.none),
+              fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+              filled: true,
+              prefixIcon: Icon(Icons.person)),
+        ),
+        SizedBox(height: 10),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Password",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none),
+            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+            filled: true,
+            prefixIcon: Icon(Icons.person),
+          ),
+          obscureText: true,
+        ),
+        SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text(
+            "SIGN IN",
+            style: TextStyle(fontSize: 20),
+          ),
+          style: ElevatedButton.styleFrom(
+            shape: StadiumBorder(),
+            padding: EdgeInsets.symmetric(vertical: 16),
+          ),
+        )
+      ],
+    );
+  }
+
+  _forgotPassword(context) {
+    return Column(
+      children: [
+        Image.asset(
+          'assets/images/ziskaPharmaIcon.png',
+          height: 100,
+          width: 100,
+        ),
+        Text(
+          'Ziska Pharmaceuticals Ltd.',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+        ),
+      ],
+    );
+  }
+
+  _signup(context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset('assets/images/alifsoft.PNG'),
+        Text(
+          "Developed By : Alif Soft ",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+        ),
+        Text(
+          "Mobile No : +88-01817042056 ",
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+        ),
+      ],
     );
   }
 }
