a546c6e5b62acf04de5b4f9a073f4be6c011ad07
Rafsan <Rafsan@DESKTOP>
2024-06-07 13:00:06 +0600

    Add territory model

--- lib/model/territoryInfoModel.dart
+++ lib/model/territoryInfoModel.dart
@@ -11,6 +11,14 @@ class TerritoryModel {
   final String teryDepotCode;
   final String teryDepotName;
 
+  final String muid;
+  final double userComID;
+  final String userComCode;
+  final String userComName;
+  final double userSysId;
+  final String userCUID;
+  final String teryStatus;
+
   TerritoryModel({
     required this.userUID,
     required this.teryCode,
@@ -21,11 +29,18 @@ class TerritoryModel {
     required this.teryRegionName,
     required this.teryDepotCode,
     required this.teryDepotName,
+    required this.muid,
+    required this.userComID,
+    required this.userComCode,
+    required this.userComName,
+    required this.userSysId,
+    required this.userCUID,
+    required this.teryStatus,
   });
 
   factory TerritoryModel.fromJson(Map<String, dynamic> json) {
     // print('in jsonnnnnnnnnnnnnnnnnnnnnnnnnn');
-    // print(json);
+    // print(json); user_SysId
     return TerritoryModel(
       userUID: json['user_UID'] ?? '',
       teryCode: json['tery_Code'] ?? '',
@@ -36,6 +51,13 @@ class TerritoryModel {
       teryRegionName: json['tery_RegionName'] ?? '',
       teryDepotCode: json['tery_DepotCode'] ?? '',
       teryDepotName: json['tery_DepotName'] ?? '',
+      muid: json['user_MUID'] ?? '',
+      userComID: json['user_ComID'].toDouble() ?? 0,
+      userComCode: json['user_ComCode'] ?? '',
+      userComName: json['user_ComName'] ?? '',
+      userSysId: json['user_SysId'] ?? 0,
+      userCUID: json['user_CUID'] ?? '',
+      teryStatus: json['tery_Status'] ?? '',
     );
   }
 }
